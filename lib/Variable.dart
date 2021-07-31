f633dd762d867a9d342e6c694084896fa391096d
souvik <souvikghosh687@gmail.com>
2021-07-31 14:29:07 +0530

    update

--- lib/Variable.dart
+++ lib/Variable.dart
@@ -6,28 +6,29 @@ const String TWITTER_URL = "https://twitter.com/iamsouviki";
 const String INSTAGRAM_URL = "https://www.instagram.com/iamsouviki";
 const String FACEBOOK_URL = "https://fb.com/iamsouviki";
 
-
 //INTRODUCTION PAGE VARIABLE
 const String NAME = "Souvik Ghosh";
-const String PROFESSION_DETAILS = 'Software Developer and Final Year Computer Science Engineering Student';
-const String MORE_DETAILS_INTRODUCTION = "I love to build neat, cool and scalable application and I'm an aspiring Full Stack engineer.\n"
+const String PROFESSION_DETAILS =
+    'Software Developer and Computer Science Graduate';
+const String MORE_DETAILS_INTRODUCTION =
+    "I love to build neat, cool and scalable application and I'm an aspiring Full Stack engineer.\n"
     'I love to learn and build new stuff that are beneficial to the community and cool to work on.\n'
     'I also have great interest in the open source community.';
-
-
 
 //IMAGE ASSET PATH
 const String IMAGE_PATH = 'assets/images/souvikimagepassport.jpg';
 
 //ABOUT ME SECTION
-const String WHO_AM_I ="Hello! I'm Souvik Ghosh , an Computer Science and Engineering students and software Developer.\n"
+const String WHO_AM_I =
+    "Hello! I'm Souvik Ghosh , an Computer Science and Engineering students and software Developer.\n"
     'I love to create performant and interesting stuff that are benificial to the community. I enjoy learning and exploring new areas in the technologies. \nIn my opinion,'
     'getting your hands dirty is the best way to learn a new skill.\n'
     'My area of interest are Cross Platform Application Development , Android Application Development and Full Stack Development\n'
     'Beside Programming, I love playing cricket and watching cricket matches with a mug full of coffee.';
 
 //EDUCATION
-const String DEGREE = "Bachelor of Technology in Computer Science & Engineering ( 2017-2021 ) ";
+const String DEGREE =
+    "Bachelor of Technology in Computer Science & Engineering ( 2017-2021 ) ";
 const String COLLEGE_NAME = "Saroj Mohan Institute Of Technology";
 const String CGPA = "CGPA :- 7.76";
 
@@ -37,27 +38,10 @@ const String TWELVETH_PERCENTAGE = "Percentage :- 73.6";
 const String TENTH_SCHOOL = "PURULIA MAHENDRA VIDYAPITH";
 const String TENTH_PERCENTAGE = "Percentage :- 85.71";
 
-
 //SKILLS
 const String SKILL1 = "JAVA ( Programming & Android )";
 const String SKILL2 = "PYTHON ( Programming )";
-const String SKILL3 = "FLUTTER ( DART )" ;
-const String SKILL4 = "GIT" ;
-const String SKILL5 = "SQLite" ;
+const String SKILL3 = "FLUTTER ( DART )";
+const String SKILL4 = "GIT";
+const String SKILL5 = "SQLite";
 const String SKILL6 = "FIREBASE";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
